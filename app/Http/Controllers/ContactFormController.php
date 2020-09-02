cb3936d76816235b6b788ef753c5e06fa792ebe3
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-02 13:48:28 +0900

    Query builder

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\ContactForm;
 
 class ContactFormController extends Controller
@@ -14,7 +15,16 @@ class ContactFormController extends Controller
      */
     public function index()
     {
-        return view('contact.index');
+        //$contact = ContactForm::all();
+        
+        $contacts = DB::table('contact_forms')
+                    ->select('id', 'your_name', 'title', 'created_at')
+                    ->orderBy('created_at', 'desc')
+                    ->get();
+        
+        //dd($contacts;
+
+        return view('contact.index', compact('contacts'));
     }
 
     /**
