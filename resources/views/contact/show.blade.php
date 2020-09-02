497fd61de5dbbb8c037be48b41fde987ea4bd8ad
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-02 15:09:00 +0900

    edit

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -21,10 +21,10 @@
                     {{ $gender }}
                     {{ $age }}
                     {{ $contact->contact }}
-                    <form method="GET" action="">
+                    <form method="GET" action="{{ route('contact.edit', ['id' => $contact->id ])}}">
                     @csrf
                     
-                    <input class="btn btn-info" type="submit" value="登録する"> 
+                    <input class="btn btn-info" type="submit" value="変更する"> 
                     </form>
                     
                 </div>
