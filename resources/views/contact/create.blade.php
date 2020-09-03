68ab326e2e84aa9b915866f6abb87fb46ca49841
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2020-09-04 00:26:44 +0900

    create form layout

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -44,32 +44,37 @@
                     <br>
 
                     <div class="form-group row">
-                      <label for="exampleInputEmail1" class="col-md-3 col-form-label text-md-right">メールアドレス</label>
+                      <label for="inputEmail" class="col-md-3 col-form-label text-md-right">メールアドレス</label>
                       <div class="col-md-9">
                         <input type="email" class="form-control" name="email">
                       </div>
                     </div>
                     <br>
 
-                    ホームページ
-                    <input type="url" name="url">
+                    <div class="form-group row">
+                      <label for="inputText" class="col-md-3 col-form-label text-md-right">ホームページ</label>
+                      <div class="col-md-9">
+                        <input type="url" class="form-control" name="url">
+                      </div>
+                    </div>
                     <br>
                     
 
                     <div class="form-group row">
                       <label for="exampleInputEmail1" class="col-md-3 col-form-label text-md-right">性別</label>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input text-md-center" type="radio" name="gender" value="0">男性</input>
-                      </div>
-                      <div class="form-check form-check-inline">
-                        <input class="form-check-input" type="radio" name="gender" value="1">女性</input>
+                      <div class="col-md-9">
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input text-md-center" type="radio" name="gender" value="0">男性</input>
+                        </div>
+                        <div class="form-check form-check-inline">
+                          <input class="form-check-input" type="radio" name="gender" value="1">女性</input>
+                        </div>
                       </div>
                     </div>
                     <br>
 
-                    
                     <div class="form-group row">
-                      <label for="ageSelect" class="col-md-3 text-md-right">年齢</label>
+                      <label for="ageSelect" class="col-md-3 col-form-label text-md-right">年齢</label>
                       <div class="col-md-9">
                         <select class="form-control" name="age">
                           <option value="">選択して下さい</option>
@@ -84,12 +89,14 @@
                     </div>
                     <br>
 
-                    お問い合わせ内容
-                    <textarea name="contact"></textarea>
+                    <div class="form-group">
+                      <label for="inputTextarea" class="col-md-3 col-form-label text-md-right">お問い合わせ内容</label>
+                      <textarea class="form-control" name="contact"></textarea>
+                    </div>
                     <br>
 
                     <div class="form-group row">
-                      <div class="col-md-3 col-form-label text-md-right">注意事項に同意する</div>
+                      <label for="col-md-3 col-form-label text-md-right">注意事項に同意する</label>
                       <div class="col-md-9">
                         <div class="form-check">
                           <input type="checkbox" class="form-check-input" name="caution" value="1">
